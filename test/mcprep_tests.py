9e9876fc4ef5f84b673a2530c87ba681a10cb6e1
Mahid Sheikh <mahid@standingpad.org>
2024-05-31 02:11:41 -0500

    test: Remove xgettext-replacement test
    Signed-off-by: Mahid Sheikh <mahid@standingpad.org>

--- test/mcprep_tests.py
+++ test/mcprep_tests.py
@@ -35,19 +35,21 @@
 # Git operations will have type checking ignored due to Mypy not being able
 # to handle cls
 
+import hashlib
+import multiprocessing as mp
+import shutil
+import sys
 import unittest
-from unittest import mock
+import urllib.request
 from io import StringIO
+from multiprocessing.managers import DictProxy
+from pathlib import Path
+from unittest import mock
+
+from git import Repo
+
 import bpy_addon_build as bab
 from bpy_addon_build.build_context.install import get_paths
-from pathlib import Path
-from git import Repo
-import shutil
-import urllib.request
-import hashlib
-import sys
-import multiprocessing as mp
-from multiprocessing.managers import DictProxy
 
 # parent folder of the tests
 TEST_FOLDER = Path(__file__).parent
@@ -55,9 +57,9 @@ VERSIONS = [2.8, 3.4, 3.5]
 MCPREP_REPO = f"{TEST_FOLDER}/MCprep"
 
 # The commit to clone from
-MCPREP_SHA = "a95b7e9fb3ac229562897892a8f811bcedafe468"
+MCPREP_SHA = "19da433ec7145f3312884df5b6950205ad96d241"
 MCPREP_URL = "https://github.com/Moo-Ack-Productions/MCprep.git"
-MCPREP_BRANCH = "milestone-3-6-0"
+MCPREP_BRANCH = "dev"
 
 POLIB_SHA = "cc05cfd048cb85031b51aef8c5c720fa9930624b"
 POLIB_URL = f"https://raw.githubusercontent.com/izimobil/polib/{POLIB_SHA}/polib.py"
@@ -205,53 +207,6 @@ class TestBpyBuildProd(unittest.TestCase):
         stdout_list = mock_stdout.getvalue().split("\n")
         self.assertIn("Building MO files...", stdout_list)
         self.assertIn("Building Translations...", stdout_list)
-
-    @mock.patch("sys.stdout", new_callable=StringIO)
-    def test_hook_patch(self, mock_stdout: StringIO) -> None:
-        """Perform a test build of MCprep at the path defined in MCPREP_REPO,
-        but on the xgettext-replacement-action branch, patched with {TEST_FOLDER}/mcprep-patches/build_pot_prebuild.patch.
-
-        This is to make sure hooks can be migrated easily
-        with simple changes.
-
-        This test will check for:
-        - stage-1 in build/
-        - MCprep_addon.zip in build/
-        - MCprep installed in the right paths
-        - mcprep.pot in MCPREP_REPO/MCprep_addon/MCprep_resources/Languages/
-        """
-
-        # Switch branch and patch
-        TestBpyBuildProd.repo.git.checkout("-f", "xgettext-replacement-action")  # type: ignore
-        TestBpyBuildProd.repo.git.apply(  # type: ignore
-            f"{TEST_FOLDER}/mcprep-patches/build_pot_prebuild.patch"
-        )
-
-        with mock.patch(
-            "sys.argv",
-            [
-                "bab",
-                "-c",
-                f"{MCPREP_REPO}/bpy-build.yaml",
-                "-b",
-                "xgettext-replacement-action",
-            ],
-        ):
-            bab.main()
-
-        self.assertTrue(Path(f"{MCPREP_REPO}/build").exists())
-        self.assertTrue(Path(f"{MCPREP_REPO}/build/MCprep_addon.zip").exists())
-
-        # Check if the MCprep addon is installed in the right paths
-        for version in get_paths(VERSIONS):
-            self.assertTrue(Path(f"{version}/MCprep_addon").exists())
-
-        # Check if the mcprep.pot is in the right path
-        self.assertTrue(
-            Path(
-                f"{MCPREP_REPO}/MCprep_addon/MCprep_resources/Languages/mcprep.pot"
-            ).exists()
-        )
 
 
 if __name__ == "__main__":
