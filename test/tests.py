2005f4879eb42268a339bb244f813bf881e8aca9
Maryam Sheikh <mahid@standingpad.org>
2025-11-05 18:28:33 -0600

    tests: Add test for depends_on
    Signed-off-by: Maryam Sheikh <mahid@standingpad.org>

--- test/tests.py
+++ test/tests.py
@@ -195,14 +195,7 @@ class TestBpyBuild(unittest.TestCase):
         with dev
 
         This test will check for:
-        - Build folder
-        - MCprep_addon.zip
-        - stage-1 folder
-        - stage-1/MCprep_addon/mcprep_dev.txt
-        - Lack of stage-1/MCprep_addon/ignore.blend
-        - "DEV MAIN" in mock_stdout
-        - "MAIN" in mock_stdout
-        - "hi guys c:" in stage-1/MCprep_addon/mcprep_dev.txt
+        - "depends_dev depends on dev, and thus must be ran after dev!"
         """
         with mock.patch(
             "sys.argv",
@@ -211,29 +204,18 @@ class TestBpyBuild(unittest.TestCase):
                 "-c",
                 f"{TEST_FOLDER}/test_addon/bpy-build.yaml",
                 "-b",
-                "dev",
                 "depend_dev",
+                "dev",
             ],
         ):
-            bab.main()
-        build = Path(f"{TEST_FOLDER}/test_addon/build")
-
-        # This could be consolidated into a single call,
-        # but I feel this is more readable as it's calling
-        # for each individual condition, and reduces complexity.
-        self.assertTrue(build.exists() and build.is_dir())
-        self.assertTrue((build / "MCprep_addon.zip").exists())
-        self.assertTrue((build / "stage-1").exists())
-        self.assertTrue((build / "stage-1/MCprep_addon/mcprep_dev.txt").exists())
-        self.assertFalse((build / "stage-1/MCprep_addon/ignore.blend").exists())
-
-        # Check mock_stdout and mcprep_dev.txt for some
-        # expected strings.
-        self.assertRegex(mock_stdout.getvalue(), r"DEV MAIN")  # dev action
-        self.assertRegex(mock_stdout.getvalue(), r"MAIN")  # default action
-
-        with open(build / "stage-1/MCprep_addon/mcprep_dev.txt", "r") as f:
-            self.assertEqual(f.read().strip(), "hi guys c:")
+            try:
+                bab.main()
+            except SystemExit:
+                # Pass through
+                pass
+        self.assertRegex(
+            mock_stdout.getvalue(), r"dev required to run depend_dev"
+        )  # default action
 
     @mock.patch("sys.stdout", new_callable=StringIO)
     def test_depend_on_fail(self, mock_stdout: StringIO) -> None:
